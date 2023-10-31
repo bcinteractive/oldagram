9cee33e2fbcf023dbf686bd0d4c981d764a5d5d5
bcinteractive <bcinteractive@users.noreply.github.com>
2023-10-31 22:55:54 +0800

    Initial upload from file

---
+++ index.js
@@ -0,0 +1,44 @@
+const posts = [
+    {
+        name: "Vincent van Gogh",
+        username: "vincey1853",
+        location: "Zundert, Netherlands",
+        avatar: "images/avatar-vangogh.jpg",
+        post: "images/post-vangogh.jpg",
+        comment: "just took a few mushrooms lol",
+        likes: 21
+    },
+    {
+        name: "Gustave Courbet",
+        username: "gus1819",
+        location: "Ornans, France",
+        avatar: "images/avatar-courbet.jpg",
+        post: "images/post-courbet.jpg",
+        comment: "i'm feelin a bit stressed tbh",
+        likes: 4
+    },
+        {
+        name: "Joseph Ducreux",
+        username: "jd1735",
+        location: "Paris, France",
+        avatar: "images/avatar-ducreux.jpg",
+        post: "images/post-ducreux.jpg",
+        comment: "gm friends! which coin are YOU stacking up today?? post below and WAGMI!",
+        likes: 152
+    }
+]
+
+// const postEl = document.getElementById("post-el")
+// const postImgEl = document.createElement("img")
+// const postAvatarEl = document.createElement("img")
+// setElement(postAvatarEl, posts[0].avatar, "Post Avatar", "post-avatar-el")
+// postEl.append(postAvatarEl)
+// setElement(postImgEl, posts[0].post, "Main Post Image", "main-post-el")
+// postEl.append(postImgEl)
+
+// function setElement(el, src, alt, id) {
+//     el.setAttribute("src", src)
+//     el.setAttribute("alt", alt)
+//     el.setAttribute("id", id)
+// }
+
